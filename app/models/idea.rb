a847b74e2122729df4b67154c552b0394a9a53c0
Sadiq Okocha <sokocha@gmail.com>
2015-01-17 15:56:17 +0000

    adds length validations for the name and description fields in idea form

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,7 +2,10 @@ class Idea < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   validates :title, presence: true
+  validates :title, length: {in: 5..45}
+
   validates :description, presence: true
+  validates :description, length: {in: 180..500}
   validates :main_image, presence: true
   validates :video_link, presence: true
   validates :category_id, presence: true
