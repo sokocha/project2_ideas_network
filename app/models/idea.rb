2f8324bbfe27623b6d1c4f21c77777e8cdbd0c05
Sadiq Okocha <sokocha@gmail.com>
2015-01-17 15:50:04 +0000

    adds presence validations for idea form

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,18 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :main_image, presence: true
+  validates :video_link, presence: true
+  validates :category_id, presence: true
+  # validate :select_category, on: :create
+
+  #     def select_category
+  #       errors.add(:category_id, "is not active") unless @idea = nil
+  #     end
+
+
 
 
 end
