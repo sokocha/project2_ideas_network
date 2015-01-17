28856f826e0a777ca7617763e16d4b984256b875
Sadiq Okocha <sokocha@gmail.com>
2015-01-17 13:05:03 +0000

    removes Category view for create, destroy and update

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,22 +3,22 @@ class IdeasController < ApplicationController
   # Lisa: if you remove the except: :index you get stuck in a redirect loop. I believe this is because of the index being the root route.
   load_and_authorize_resource except: :index
   
-  # def new
-  # end
+  def new
+  end
 
-  # def show
-  # end
+  def show
+  end
 
-  # def index
-  # end
+  def index
+  end
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def update
-  # end
+  def update
+  end
 
-  # def create
-  # end
+  def create
+  end
 
 end
