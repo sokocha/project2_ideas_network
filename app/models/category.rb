9230433dbb7bd3e35a394efcd2c29c1e49c19926
Sadiq Okocha <sokocha@gmail.com>
2015-01-17 16:19:10 +0000

    adds validation for creating a category. A category name must exist

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :ideas
+  validates :name, presence: true
   
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,19 +3,13 @@ class Idea < ActiveRecord::Base
   belongs_to :category
   validates :title, presence: true
   validates :title, length: {in: 5..45}
+  validates :title, uniqueness: true
 
   validates :description, presence: true
-  validates :description, length: {in: 180..500}
+  validates :description, length: {maximum: 500}
   validates :main_image, presence: true
   validates :video_link, presence: true
   validates :category_id, presence: true
-  # validate :select_category, on: :create
-
-  #     def select_category
-  #       errors.add(:category_id, "is not active") unless @idea = nil
-  #     end
-
-
 
 
 end
