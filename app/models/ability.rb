a293dddbe62ebceb3a18415266db691e628c6f89
Sadiq Okocha <sokocha@gmail.com>
2015-01-17 12:22:07 +0000

    adds users permissions to abilities model for create idea, update idea if belongs to user. Also allows user to read all

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,11 @@ class Ability
       can :manage, :all
     else
       # can :read, Idea -- Lisa: will need to update this with chosen levels of permission. Currently everyone is effectively an admin
-      can :manage, :all
+      can :read, :all
+      can :create, Idea
+      can :update, Idea do |idea|
+        idea.try(:user) == user
+      end
     end
   end
 
