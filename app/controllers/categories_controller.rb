a3059a67a0834422abbebb21f4a0b94350dfc2a6
Sadiq Okocha <sokocha@gmail.com>
2015-01-17 13:06:27 +0000

    adds actions for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,24 +1,71 @@
 class CategoriesController < ApplicationController
 
+ def index
+   @categories = Category.all
+ end
 
-  def new
-  end
+   # GET /categories/1
+   # GET /categories/1.json
+   def show
+   end
 
-  def edit
-  end
+   # GET /categories/new
+   def new
+     @category = Category.new
+   end
 
-  def delete
-  end
+   # GET /categories/1/edit
+   def edit
+   end
 
-  def create
-  end
+   # POST /categories
+   # POST /categories.json
+   def create
+     @category = Category.new(category_params)
 
-  def update
-  end
+     respond_to do |format|
+       if @category.save
+         format.html { redirect_to @category, notice: 'Category was successfully created.' }
+         format.json { render :show, status: :created, location: @category }
+       else
+         format.html { render :new }
+         format.json { render json: @category.errors, status: :unprocessable_entity }
+       end
+     end
+   end
 
-  def show
-  end
+   # PATCH/PUT /categories/1
+   # PATCH/PUT /categories/1.json
+   def update
+     respond_to do |format|
+       if @category.update(category_params)
+         format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+         format.json { render :show, status: :ok, location: @category }
+       else
+         format.html { render :edit }
+         format.json { render json: @category.errors, status: :unprocessable_entity }
+       end
+     end
+   end
 
-  def index
-  end
-end
+   # DELETE /categories/1
+   # DELETE /categories/1.json
+   def destroy
+     @category.destroy
+     respond_to do |format|
+       format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+       format.json { head :no_content }
+     end
+   end
+
+   private
+     # Use callbacks to share common setup or constraints between actions.
+     def set_category
+       @category = Category.find(params[:id])
+     end
+
+     # Never trust parameters from the scary internet, only allow the white list through.
+     def category_params
+       params.require(:category).permit(:name, :is_live)
+     end
+   end
