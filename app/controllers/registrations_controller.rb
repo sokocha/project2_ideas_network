60e6e71d0d4c337db7a44d122931b84de856219c
Sadiq Okocha <sokocha@gmail.com>
2015-01-19 18:01:44 +0000

    adds user_image to registerations controller's BOTH actions

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
  
   def sign_up_params
-    params.require(:user).permit(:username, :bio, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :bio, :email, :password, :password_confirmation, :user_image)
   end
  
   def account_update_params
