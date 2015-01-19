48d5a74b345d98a4d80acb7a5cf6cb8f51f7e4e3
Sadiq Okocha <sokocha@gmail.com>
2015-01-19 18:22:08 +0000

    adds idea_image to Ideas controller's strong params

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -56,7 +56,7 @@ class IdeasController < ApplicationController
 
 private
   def idea_params
-    params.require(:idea).permit(:title, :description, :category_id, :main_image, :video_link, :feedback_type)
+    params.require(:idea).permit(:title, :description, :category_id, :main_image, :video_link, :feedback_type, :idea_image)
   end
 
 end
