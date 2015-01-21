da3850ec52cc75e6d892fc77d2d53c27309299de
Lisa Pollack <lisa.s.pollack@gmail.com>
2015-01-21 12:15:35 +0000

    adds to create action in controller to include validations and includes styling of errors in scaffold style

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -10,7 +10,7 @@ class Idea < ActiveRecord::Base
 
   validates :description, presence: true
   validates :description, length: {maximum: 750}
-  validates :main_image, presence: true
+  # validates :main_image, presence: true
   validates :video_link, presence: true
   validates :category_id, presence: true
 
